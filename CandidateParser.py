73c92d84fde5332516960a4326cee56b566e48c6
Darren Cotterill <darren.cotterill@gmail.com>
2016-06-27 14:08:37 +1000

    Initial commit

---
+++ CandidateParser.py
@@ -0,0 +1,122 @@
+import csv
+import collections
+
+class CandidateParser:
+
+    def parseReaderStages(self, reader):
+        data = {}
+
+        stages = {}
+        decisions = {}
+
+        for row in reader:
+            stages[row[6]] = 1
+            decisions[row[7]] = 1
+            if not (row[6] == 'Make Offer' and row[7] == 'Declined Offer') \
+                and not (row[6] == 'Case Study' and row[7] == 'Withdrawn') \
+                and not (row[6] == 'Case Study' and row[7] == 'Rejected'):
+                try:
+                    candidate = data[row[0]]
+                    candidate["stages"].append({"stage":row[6], "decision-status": row[7],
+                                             "change-person": row[10], "change-date": row[11], "stage-coordinator": row[12]})
+                except KeyError:
+                    candidate = {"name": row[1], "email": row[2], "phone": row[3], "opening": row[5],
+                                 "stages": [{"stage":row[6], "decision-status": row[7],
+                                             "change-person": row[10], "change-date": row[11], "stage-coordinator": row[12]}]}
+                    data[row[0]] = candidate
+
+                # if row[11] == "":
+                #     print row
+
+                if row[6] == "Make Offer": print "Offer:" + row[1] + ": " + row[11]
+                if row[6] == "Prepare Contract": print "Contract:" + row[1] + ": " + row[11]
+
+        # print "Stages ---------"
+        # orderedStages = collections.OrderedDict(sorted(stages.items()))
+        # for key, stage in orderedStages.iteritems():
+        #     print key
+        # print "Decisions ------"
+        # for key, decision in decisions.iteritems():
+        #     print key
+        return data
+
+
+    def stage_count_per_month(self, data_per_month, key, stages, candidate, stage):
+        count_per_month = data_per_month.setdefault(key, {})
+        stage_description = stage["stage"]
+        if (stage_description in stages):
+
+            month = stage["change-date"][0:7]
+            try:
+                people = count_per_month[month]
+            except KeyError:
+                count_per_month[month] = []
+
+            if candidate["name"] not in count_per_month[month]:
+                count_per_month[month].append(candidate["name"])
+        return data_per_month
+
+    def parseFileStages(self):
+
+        data = {}
+        # with open('data/sample.csv', 'rUb') as csvFile:
+        with open('data/data-20-06-2016.csv', 'rUb') as csvFile:
+            reader = csv.reader(csvFile)
+
+            data = self.parseReaderStages(reader)
+
+        data_per_month = {}
+
+        for key, candidate in data.iteritems():
+            for stage in candidate["stages"]:
+                data_per_month = self.stage_count_per_month(
+                    data_per_month, "phone_interviews", ["Phone Interview", "Phone screen",
+                                                 "Phone or Video screen", "4. Telephonic Screening"]
+                    , candidate, stage)
+
+                data_per_month = self.stage_count_per_month(
+                    data_per_month,"case_studies", ["Case Study"]
+                    , candidate, stage)
+
+                data_per_month = self.stage_count_per_month(
+                    data_per_month, "offers", ["Make Offer", "11. Make offer", "Prepare Contract"]
+                    , candidate, stage)
+
+                data_per_month = self.stage_count_per_month(
+                    data_per_month, "competency", ["1st Interview - CV, Cultural Fit & Aptitude",
+                                               "1st Interview- Background Interview",
+                                               "1st Interview-Aptitude&BA test",
+                                               "2nd Interview - Competency",
+                                               "2nd Interview- Competency Assessment",
+                                               "BG\CF Int",
+                                               "Background/Experience/Fit",
+                                               "Competency",
+                                               "Competency Assessment",
+                                               "Competency Interview & Aptitude Test",
+                                               "Competency\Background Int & Aptitude"]
+                    , candidate, stage)
+
+        print "Phone Interviews Per Month"
+        orderedOffers = collections.OrderedDict(sorted(data_per_month["case_studies "].items()))
+        report = "month, phone_interviews, competency, case_studies, offers\n"
+        for month, offerOwners in orderedOffers.iteritems():
+            report = report + month + ", " \
+                  + str(len(data_per_month["phone_interviews"].get(month, ""))) + ", " \
+                  + str(len(data_per_month["competency"].get(month, ""))) + ", " \
+                  + str(len(data_per_month["case_studies"].get(month, ""))) + ", " \
+                  + str(len(data_per_month["offers"].get(month, ""))) + ", " \
+                  + str(offerOwners) + "\n"
+
+        return data, report
+
+def main():
+    parser = CandidateParser()
+    data, report = parser.parseFileStages()
+
+    f = open("report.csv", "w")
+    f.write(report)
+    f.close()
+
+if __name__ == '__main__':
+    main()
+
---
+++ CandidateParserTests.py
@@ -0,0 +1,68 @@
+import unittest
+import csv
+import StringIO
+from CandidateParser import CandidateParser
+
+
+class CandidateParserTests(unittest.TestCase):
+
+    def testSingleCandidateSingleStage(self):
+
+        header = "Candidate ID,Candidate Name,Candidate Email,Candidate Phone,Opening ID,Opening,Stage,Decision Status," \
+             + "Decision Reason,Reason Description,Person who changed the stage,Date of change,Stage Coordinator\n"
+
+        f = StringIO.StringIO(header + "785646,Ale M,ale.m@gmail.com,0405100000,LBR80355,"
+                              + "Consulting Business Analyst (Consultant/Senior/Lead/Principal levels),"
+                              + "4a. Informal Chat,In Process,,,Mel B,2016-03-29,Tom J\n")
+
+        reader = csv.reader(f)
+        p = CandidateParser()
+
+        data = p.parseReaderStages(reader)
+
+        self.assertEqual(data["785646"], {"name":"Ale M", "email":"ale.m@gmail.com",
+                                          "phone":"0405100000",
+                                          "opening":"Consulting Business Analyst (Consultant/Senior/Lead/Principal levels)",
+                  "stages":[{"stage":"4a. Informal Chat", "decision-status": "In Process",
+                             "change-person": "Mel B", "change-date":"2016-03-29", "stage-coordinator":"Tom J"}]})
+
+    def testSingleCandidateThreeStages(self):
+
+        self.maxDiff = None
+
+        header = "Candidate ID,Candidate Name,Candidate Email,Candidate Phone,Opening ID,Opening,Stage,Decision Status," \
+             + "Decision Reason,Reason Description,Person who changed the stage,Date of change,Stage Coordinator\n"
+
+        f = StringIO.StringIO(header + "785646,Ale M,ale.m@gmail.com,0405100000,LBR80355,"
+                              + "Consulting Business Analyst (Consultant/Senior/Lead/Principal levels),"
+                              + "4a. Informal Chat,In Process,,,Mel B,2016-03-29,Tom J\n"
+
+                              + "785646,Ale M,ale.m@gmail.com,0405100000,LBR80355,"
+                              + "Consulting Business Analyst (Consultant/Senior/Lead/Principal levels),"
+                              + "5. Formal Chat,In Process,,,Diane Cotterill,2016-04-10,Tom J\n"
+
+                              + "785646,Ale M,ale.m@gmail.com,0405100000,LBR80355,"
+                              + "Consulting Business Analyst (Consultant/Senior/Lead/Principal levels),"
+                              + "6. Offer,Hired,,,Mel B,2016-05-01,Tom J\n")
+
+        reader = csv.reader(f)
+        p = CandidateParser()
+
+        data = p.parseReaderStages(reader)
+
+        self.assertEqual(data["785646"], {"name":"Ale M", "email":"ale.m@gmail.com",
+                                          "phone":"0405100000",
+                                          "opening":"Consulting Business Analyst (Consultant/Senior/Lead/Principal levels)",
+                  "stages":[{"stage":"4a. Informal Chat", "decision-status": "In Process",
+                             "change-person": "Mel B", "change-date":"2016-03-29", "stage-coordinator":"Tom J"},
+                            {"stage":"5. Formal Chat", "decision-status": "In Process",
+                             "change-person": "Diane Cotterill", "change-date":"2016-04-10", "stage-coordinator":"Tom J"},
+                            {"stage":"6. Offer", "decision-status": "Hired",
+                             "change-person": "Mel B", "change-date":"2016-05-01", "stage-coordinator":"Tom J"}]})
+
+def main():
+
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
